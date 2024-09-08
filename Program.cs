9e8e74e0da3c9b2af6452eda75536507ba6b7353
John Tolen <jwtolen@crimson.ua.edu>
2024-09-08 14:05:31 -0500

    https://github.com/UAMIS221-321/mis-221-lab-2-jwtolen

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿//initialize variables
+using System.Diagnostics;
+using System.Runtime.Intrinsics.Arm;
+
+int numberOfSandwiches = 0;
+int numberOfToppings = 0;
+double tip = 0;
+double totalSandwichCost;
+double totalToppingCost;
+double baseCost;
+
+//initialize constants
+const double COST_OF_SANDWICH = 4.75;
+const double COST_OF_TOPPING = 0.55;
+const double DISCOUNT_AMOUNT = 0.10;
+
+//prompt the user for # of sandwiches
+System.Console.WriteLine("How many sandwiches?");
+numberOfSandwiches = int.Parse(Console.ReadLine());
+System.Console.WriteLine("Sandwiches "+numberOfSandwiches);
+
+//prompt user for # of toppings needed
+System.Console.WriteLine("How many toppings?");
+numberOfToppings = int.Parse(Console.ReadLine());
+System.Console.WriteLine("Toppings "+numberOfToppings);
+
+//prompt user for the tip
+System.Console.WriteLine("How much would you like to tip?");
+tip = double.Parse(Console.ReadLine());
+System.Console.WriteLine("Tip "+tip);
+
+//calculate base cost
+totalSandwichCost = COST_OF_SANDWICH * numberOfSandwiches;
+totalToppingCost = COST_OF_TOPPING * numberOfToppings;
+baseCost = totalSandwichCost + totalToppingCost;
+
+//incorporate tip and discount
+double discountedCost = baseCost * (1 - DISCOUNT_AMOUNT);
+double orderCost = discountedCost + tip;
+
+//Display final price
+System.Console.WriteLine("Total Price: "+orderCost.ToString("F2"));
